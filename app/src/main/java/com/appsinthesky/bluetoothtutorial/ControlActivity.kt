ae814bfa1769326445b5b6e15d5218767c9cd474
liam smith <liam.smith@appsinthesky.com>
2018-01-16 14:25:30 +0000

    - full project commit

---
+++ app/src/main/java/com/appsinthesky/bluetoothtutorial/ControlActivity.kt
@@ -0,0 +1,103 @@
+package com.appsinthesky.bluetoothtutorial
+
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.util.*
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import kotlinx.android.synthetic.main.control_layout.*
+import java.io.IOException
+
+
+
+class ControlActivity: AppCompatActivity() {
+
+    companion object {
+        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var m_bluetoothSocket: BluetoothSocket? = null
+        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnected: Boolean = false
+        lateinit var m_address: String
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.control_layout)
+        m_address = intent.getStringExtra(SelectDeviceActivity.EXTRA_ADDRESS)
+
+        ConnectToDevice(this).execute()
+
+        control_led_on.setOnClickListener { sendCommand("a") }
+        control_led_off.setOnClickListener { sendCommand("b") }
+        control_led_disconnect.setOnClickListener { disconnect() }
+    }
+
+    private fun sendCommand(input: String) {
+        if (m_bluetoothSocket != null) {
+            try{
+                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            } catch(e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_isConnected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()
+    }
+
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSuccess: Boolean = true
+        private val context: Context
+
+        init {
+            this.context = c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            m_progress = ProgressDialog.show(context, "Connecting...", "please wait")
+        }
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            try {
+                if (m_bluetoothSocket == null || !m_isConnected) {
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException) {
+                connectSuccess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSuccess) {
+                Log.i("data", "couldn't connect")
+            } else {
+                m_isConnected = true
+            }
+            m_progress.dismiss()
+        }
+    }
+}
---
+++ app/src/main/java/com/appsinthesky/bluetoothtutorial/SelectDeviceActivity.kt
@@ -0,0 +1,82 @@
+package com.appsinthesky.bluetoothtutorial
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.select_device_layout.*
+import org.jetbrains.anko.toast
+
+class SelectDeviceActivity : AppCompatActivity() {
+
+    private var m_bluetoothAdapter: BluetoothAdapter? = null
+    private lateinit var m_pairedDevices: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.select_device_layout)
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(m_bluetoothAdapter == null) {
+            toast("this device doesn't support bluetooth")
+            return
+        }
+        if(!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+
+        select_device_refresh.setOnClickListener{ pairedDeviceList() }
+
+    }
+
+    private fun pairedDeviceList() {
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+
+        if (!m_pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                list.add(device)
+                Log.i("device", ""+device)
+            }
+        } else {
+            toast("no paired bluetooth devices found")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = list[position]
+            val address: String = device.address
+
+            val intent = Intent(this, ControlActivity::class.java)
+            intent.putExtra(EXTRA_ADDRESS, address)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    toast("Bluetooth has been enabled")
+                } else {
+                    toast("Bluetooth has been disabled")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                toast("Bluetooth enabling has been canceled")
+            }
+        }
+    }
+}
